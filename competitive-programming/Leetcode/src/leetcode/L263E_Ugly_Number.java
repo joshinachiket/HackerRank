1bcd166053f199f245ac062980cbd3c658644943
Nachiket Joshi <nachiket.r.joshi@gmail.com>
2018-02-17 00:52:43 -0800

    263. Ugly Number 1

---
+++ competitive-programming/Leetcode/src/leetcode/L263E_Ugly_Number.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+public class L263E_Ugly_Number {
+	
+	public static boolean checkUglyNumber (int num) {
+		
+		// catch here is that, if the number has only 2, 3, 5 as divisors
+		// we can iteratively divide by them in while loop and arrive at 1
+		
+		if (num ==0) return false;
+		
+		while (num % 2 ==0) {
+			num = num / 2;
+		}
+		
+		while (num % 3 ==0) {
+			num = num / 3;
+		}
+		
+		while (num % 5 ==0) {
+			num = num / 5;
+		}
+		
+		if (num == 1) return true;
+		
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		// enter number to check if its ugly or not
+		int num = 11;
+		System.out.println(checkUglyNumber(num));		
+	}
+
+}
